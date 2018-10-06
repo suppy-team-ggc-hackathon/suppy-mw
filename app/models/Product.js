88f62a552cfc85fc54414b43246734e7eb0417c8
Theresa Hradilak <hradilak@uni-potsdam.de>
2018-10-06 20:19:45 +0200

    Add product type images feature

--- app/models/Product.js
+++ app/models/Product.js
@@ -1,7 +1,37 @@
+import ProductType from "./ProductType";
+import log4js from 'log4js';
+const log = log4js.getLogger('product');
+
 export default class Product {
 
     constructor(data) {
-        this._data = data
+        log.info('Product Constructor WE ARE IN')
+        log.info('Product CONSTRUCTOR >>> input_data' + JSON.stringify(data))
+        this._data = {}
+        this._data.title = data.title
+        log.info('Product Constructor input data title', this._data.title)
+        this._data.type = this.retrieveDatatype(data.title)
+        log.info('Product Constructor >>> datatype',this._data.type)
+        this._data.img = ProductType[this._data.type]
+        log.info('Product Constructor img ', this._data.img)
+    }
+
+    retrieveDatatype(title){
+        log.info('retrieveDATATYPE we are in')
+        var datatype
+
+        Object.keys(ProductType).forEach(function(type) {
+            log.info('This is the Type Hype!', type)
+            log.info(title)
+            if (title.includes(type)){
+                log.info(type)
+                datatype = type;
+                return datatype
+            }
+        });
+        datatype = "UNDEFINED"
+        return datatype
+
     }
 
     getType() {
--- app/models/ProductType.js
+++ app/models/ProductType.js
@@ -1,5 +1,6 @@
 export default {
-    MINERAL,
-    END_PRODUCT,
-    DELIVERY
+    BANANA: "https://drive.google.com/file/d/1-08P5w6U66m8iUs6MPNTnUAfARlGYYGS/view",
+    KIWI: "https://drive.google.com/file/d/1eGcDhJ9Rv1PZoHZ6mlJ65cGsUpGPvUXo/view",
+    PINEAPPLE: "https://drive.google.com/file/d/1uulXPgdNtYCDbBdVDwVWBGfCXbFsciQi/view",
+    UNDEFINED: "https://drive.google.com/file/d/1uErV3M9de00UrMiWQtvdLQmOGtzFrftq/view"
 }
